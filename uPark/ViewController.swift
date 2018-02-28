a0c5c4385ffdfb75d7c8ea0306a47190250c32bb
Kurt Robinson <kr185@uowmail.edu.au>
2018-02-28 17:02:23 +1100

    Upark pshop files
    Need to storyboard this a bit more, maybe fiddle with some ideas.

--- uPark/ViewController.swift
+++ uPark/ViewController.swift
@@ -8,17 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameTextField.delegate = self;
+        
+        
+        print("hey");
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        nameLabel.text = "Default Text";
+        
     }
+    
+    
 
 
 }
